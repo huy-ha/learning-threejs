df0c0d33eb8ca48684f7118ec5fe05457fd53802
huy-ha <haquochuy@gmail.com>
2018-10-14 19:52:22 -0400

    added bump maps

--- globe/js/globe.js
+++ globe/js/globe.js
@@ -1,15 +1,13 @@
 var camera, scene, renderer, composer, cameraControl;
 
-var cubeMaterial;
-var rotSpeed;
+var w = window.innerWidth;
+var h = window.innerHeight;
 
 var stats,statsOn;
 
-statsOn = false;
+statsOn = true;
 
 function init(){ 
-    var w = window.innerWidth;
-    var h = window.innerHeight;
 
     scene = new THREE.Scene();
     
@@ -19,11 +17,26 @@ function init(){
     renderer.shadowMap.enabled = true;
 
     camera = new THREE.PerspectiveCamera(45,w/h, 0.1, 1000);
-    camera.position.z = 33;
-    camera.position.x = 35;
-    camera.position.y = 36;
+    camera.position.z = 35;
+    camera.position.x = 21;
+    camera.position.y = 15;
     camera.lookAt(scene.position);
-    
+
+    //Create Earth
+    var sphereGeometry = new THREE.SphereGeometry(15,30,30); //radius, width segments, height segments
+    var earthMesh = new THREE.Mesh(sphereGeometry, createEarthMaterial());
+    earthMesh.name = 'earth';
+    scene.add(earthMesh);
+
+    //create Clouds
+    var cloudGeometry = new THREE.SphereGeometry(
+       sphereGeometry.parameters.radius *1.01,
+       sphereGeometry.parameters.widthSegments,
+       sphereGeometry.parameters.heightSegments
+   );
+   var cloudMesh = new THREE.Mesh(cloudGeometry, createCloudMaterial() );
+   cloudMesh.name = 'cloud';
+   scene.add(cloudMesh);
 
     cameraControl = new THREE.OrbitControls( camera ,renderer.domElement );
 
@@ -32,7 +45,6 @@ function init(){
 	this.cloudSpeed = -0.0002;
     };
 
-    addEarth();
     addLights()
     addControlGui(control);
     addStats();
@@ -42,7 +54,6 @@ function init(){
 
 function createBG(w,h){
     //Starry Background
-
     var cameraBG = new THREE.OrthographicCamera(-w,w,h,-h, -10000,10000);
     cameraBG.position.z = 50;
 
@@ -72,31 +83,19 @@ function createBG(w,h){
 }
 
 
-
-
-function addEarth(){
-    var sphereGeometry = new THREE.SphereGeometry(15,30,30); //radius, width segments, height segments
-    var earthMesh = new THREE.Mesh(sphereGeometry, createEarthMaterial());
-    earthMesh.name = 'earth';
-    scene.add(earthMesh);
-
-    //creating clouds
-   var cloudGeometry = new THREE.SphereGeometry(
-       sphereGeometry.parameters.radius*1.01,
-       sphereGeometry.parameters.widthSegments,
-       sphereGeometry.parameters.heightSegments
-   );
-   var cloudMesh = new THREE.Mesh(cloudGeometry, createCloudMaterial() );
-   cloudMesh.name = 'cloud';
-   scene.add(cloudMesh);
-
-}
-
 function createEarthMaterial(){
-    var earthTexture = new THREE.TextureLoader().load("../assets/earthmap4k.jpg");
+    var earthTexture = new THREE.TextureLoader().load("..ml/assets/earthmap10k.jpg");
     var earthMaterial = new THREE.MeshPhongMaterial();
     earthMaterial.map = earthTexture;
 
+    //normal map
+    var earthNormal = new THREE.TextureLoader().load("../assets/earth_normalmap_flat4k.jpg");
+    earthMaterial.normalMap = earthNormal;
+    earthMaterial.normalScale = new THREE.Vector2(0.5,0.7);
+
+    //specular map
+    earthMaterial.specularMap = new THREE.TextureLoader().load("../assets/earthspec4k.jpg");
+    earthMaterial.specular = new THREE.Color(0x262626);
     return earthMaterial;
 }
 
@@ -114,6 +113,7 @@ function addStats(){
         stats.domElement.style.position = "absolute";
         stats.domElement.style.left = '0px';
         stats.domElement.style.right = '0px';
+	stats.domElement.style.width = '160px';
         document.body.appendChild( stats.domElement);
     }
 }
@@ -129,10 +129,9 @@ function addLights(){
 
 window.onload = init;
 
-
 function addControlGui(controlObject){
     var gui = new dat.GUI();
-    gui.add(controlObject, "rotationSpeed", -0.01, 0.01);
+    gui.add(controlObject, "rotationSpeed", -0.005, 0.01);
     gui.add(controlObject, "cloudSpeed", -0.001,0.001);
 }
 
@@ -150,3 +149,13 @@ function render(){
     requestAnimationFrame(render);
 }
 
+function handleResize(){
+    w = window.innerWidth;
+    h = window.innerHeight;
+    camera.aspect = w/h;
+    camera.updateProjectionMatrix();
+    renderer.setSize(w,h);
+    console.log("window resized");
+}
+
+window.addEventListener('resize',handleResize,false);
